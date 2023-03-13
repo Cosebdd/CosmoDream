8228886cd371bfd53e3021d077538e67b7d16333
Cosebdd <Cosebdd@gmail.com>
2023-03-14 00:06:09 +0100

    Made a temporary fix for player jumping
    It should work fine before we implement a state machine. At least debug output doesn't get overfilled with errors anymore.

--- kinematic-objects/player/Player.gd
+++ kinematic-objects/player/Player.gd
@@ -12,17 +12,22 @@ func _physics_process(_delta):
 	if input.x < 0: body.scale.x = -1
 	if input.x > 0: body.scale.x = 1
 	
-	if is_on_floor():
+	var is_on_floor = is_on_floor();
+	if is_on_floor:
 		if Input.is_action_just_pressed("jump"):
+			animation_player.play("Jump")
 			jump()
+			is_on_floor = false;
 	else:
-		animation_player.play("Jump")
 		if Input.is_action_just_released("jump") and velocity.y <= -min_jump_height:
 			velocity.y = -min_jump_height
 		
 		handle_body_in_air()
 	
 	process(input)
+	
+	if !is_on_floor:
+		return
 	
 	if input.x == 0 and velocity.x == 0:
 		animation_player.play("Idle")
