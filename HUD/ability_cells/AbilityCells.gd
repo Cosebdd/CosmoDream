ed93bf643b3b5e9785b3b30beec7eb20c6b36ed0
nikita.vostrosablin <nikita.vostrosablin@gmail.com>
2023-03-13 22:52:21 +0100

    make HUD more general

--- HUD/ability_cells/AbilityCells.gd
+++ HUD/ability_cells/AbilityCells.gd
@@ -1,19 +1,38 @@
 extends Control
 
-
-onready var bar = $"./CanvasLayer/Ability1/VBoxContainer/HP"
-onready var panel = $"CanvasLayer/Ability1"
-
-func _ready():
-	var max_health = get_node("../Enemy").max_health
-	bar.max_value = max_health
+onready var bar_path = "CanvasLayer/Ability%s/VBoxContainer/HP"
+onready var panel_path = "CanvasLayer/Ability%s"
+onready var bars_ability = []
+onready var panels_ability = []
+var ability_cell_number = 4
 	
 
 
+func _ready():
+	# Test until there is a state of the player implemented
+	var max_health = get_node("../Enemy").max_health
 
+	
+	for i in range(1, ability_cell_number + 1):
+		bars_ability.append(get_node(bar_path%i))
+		panels_ability.append(get_node(panel_path%i))
+		
+	bars_ability[0].max_value = max_health
+	
+	
+func _process(_delta):
+	pass
 
-func _on_Enemy_health_changed(health_value):
-	bar.value = health_value
+func get_list_of_abilities() -> Array:
+	# Read list of current abilities from state
+	return []
+	
+func _on_cell_health_changed(ability, health_value):
+	bars_ability[ability].value = health_value
 	if health_value <= 0:
-		panel.visible = false
+		panels_ability[ability].visible = false
+	
+func _on_cell_weapon_changed(ability, weapon_value):
+	pass
+
 		
