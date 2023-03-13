78f26ce5a3f84737a395e079d9ef58a42cf93e02
nikita.vostrosablin <nikita.vostrosablin@gmail.com>
2023-03-13 20:05:17 +0100

    feature(HUD): make panel invisible after kill

--- HUD/ability_cells/AbilityCells.gd
+++ HUD/ability_cells/AbilityCells.gd
@@ -2,6 +2,7 @@ extends Control
 
 
 onready var bar = $"./CanvasLayer/PanelContainer/VBoxContainer/TextureProgress"
+onready var panel = $"CanvasLayer/PanelContainer"
 
 func _ready():
 	var max_health = get_node("../Enemy").max_health
@@ -13,3 +14,6 @@ func _ready():
 
 func _on_Enemy_health_changed(health_value):
 	bar.value = health_value
+	if health_value <= 0:
+		panel.visible = false
+		
