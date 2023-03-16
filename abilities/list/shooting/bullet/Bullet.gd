5f6118183f97a61fdf152fc29793248d3e60d494
Viktor Pisarenko <xperia.v.j.p@gmail.com>
2023-03-16 23:11:46 +0100

    Add shotgun

--- abilities/list/shooting/bullet/Bullet.gd
+++ abilities/list/shooting/bullet/Bullet.gd
@@ -1,19 +1,28 @@
 extends KinematicBody2D
 class_name Bullet
 
-export(int) var speed = 5
+export(int) var speed = 200
 onready var _ttl := $TTL
+onready var sprite := $Sprite
 
 var _direction = Vector2.ZERO
-
+var _delay = 0.0
 
 func _ready():
-	_ttl.start()
+	sprite.visible = false
 
 
-func _physics_process(_delta) -> void:
+func _physics_process(delta: float) -> void:
+	if _delay > 0:
+		_delay -= delta
+		return
+	
+	if _ttl.is_stopped():
+		sprite.visible = true
+		_ttl.start()
+	
 	var _velocity = move_and_slide(_direction * speed)
-	if abs(_velocity.x) < speed:
+	if _velocity.distance_to(Vector2.ZERO) < speed:
 		queue_free()
 
 
@@ -28,3 +37,7 @@ func _on_TTL_timeout():
 func set_direction(direction: Vector2) -> void:
 	_direction = direction.normalized()
 	rotation = Vector2.RIGHT.angle_to(_direction)
+	
+	
+func set_spawn_delay(delay: float) -> void:
+	_delay = delay
