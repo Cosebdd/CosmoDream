952da973c47b3b4ad1f8aee00e8a04861fc6efdf
Viktor Pisarenko <xperia.v.j.p@gmail.com>
2023-03-18 14:29:28 +0100

    Add autogenerated ability cells in HUD

--- HUD/ability-cell/AbilityCell.gd
+++ HUD/ability-cell/AbilityCell.gd
@@ -7,20 +7,34 @@ onready var picture := $HBoxContainer/Pic
 
 export(float) var box_animation_time = 0.3
 
+var _ability: Ability
 
-func init(max_hp: float, max_ep: float):
-	hp_box.max_value = max_hp
-	ep_box.max_value = max_ep
-	hp_box.value = max_hp
-	ep_box.value = max_ep
+func init(ability):
+	_ability = ability as Ability
+	hp_box.max_value = float(_ability.max_health)
+	hp_box.value = float(_ability.max_health)
+	
+	var max_ep_value = float(_ability.charge_limit) if _ability.charge_limit else 0
+	ep_box.max_value = max_ep_value
+	ep_box.value = max_ep_value
+	
+	_ability.connect("charges_updated", self, "set_ep")
+
+
+func _exit_tree():
+	_ability.disconnect("charges_updated", self, "set_ep")
+
+
+func set_picture(pic: Texture) -> void:
+	picture.texture = pic
 
 
 func set_hp(hp: float) -> void:
 	_tween_bar(hp_box, hp)
 
 
-func set_ep(ep: float) -> void:
-	_tween_bar(ep_box, ep)
+func set_ep(ep) -> void:
+	_tween_bar(ep_box, float(ep))
 
 
 func set_hp_diff(hp_diff: float) -> void:
@@ -28,13 +42,13 @@ func set_hp_diff(hp_diff: float) -> void:
 
 
 func set_ep_diff(ep_diff: float) -> void:
-	_tween_bar(ep_box, ep_box.value + ep_diff, true)
+	_tween_bar(ep_box, ep_box.value + ep_diff)
 
 
-func _tween_bar(object: TextureProgress, value: int, with_color_effect = false) -> void:
+func _tween_bar(object: TextureProgress, value: float, with_color_effect = false, time = box_animation_time) -> void:
 	var tween := create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BOUNCE)
-	tween.tween_property(object, "value", value, box_animation_time)
-	object.modulate
+	tween.tween_property(object, "value", value, time)
+	
 	if with_color_effect:
 		var color
 		if value < object.value:
@@ -44,6 +58,4 @@ func _tween_bar(object: TextureProgress, value: int, with_color_effect = false) 
 		tween.parallel()
 		tween.tween_property(object, "modulate", color, 0.15)
 		tween.tween_property(object, "modulate", Color(1,1,1,1), 0.15)
-
-
 
--- HUD/ability_cells/AbilityCells.gd
+++ HUD/ability_cells/AbilityCells.gd
@@ -1,37 +1,38 @@
 extends Control
 
-onready var bar_path = "CanvasLayer/Ability%s/VBoxContainer/HP"
-onready var panel_path = "CanvasLayer/Ability%s"
-onready var bars_ability = []
-onready var panels_ability = []
-var ability_cell_number = 4
 onready var AbilityCell = preload("res://HUD/ability-cell/AbilityCell.tscn")
 onready var container = $CanvasLayer/HBoxContainer
-var abilities = []
+var _abilities = []
+
 
 func _ready():
-	var cell = AbilityCell.instance()
-	container.add_child(cell)
-	cell.init(100.0, 7.0)
-	abilities.append(cell)
+	Events.connect("set_abilities", self, "_on_abilities_change")
 
 
 func _process(delta):
 	if Input.is_action_just_pressed("test-injury"):
-		abilities[0].set_hp_diff(-10.0)
+		_abilities[0].set_hp_diff(-10.0)
 	
 	if Input.is_action_just_pressed("test-heal"):
-		abilities[0].set_hp_diff(10.0)
+		_abilities[0].set_hp_diff(10.0)
 
 
-func get_list_of_abilities() -> Array:
-	# Read list of current abilities from state
-	return []
+func _clear_abilities() -> void:
+	for a in _abilities:
+		container.remove_child(a)
 
-func _on_cell_health_changed(ability, health_value):
-	bars_ability[ability].value = health_value
-	if health_value <= 0:
-		panels_ability[ability].visible = false
 
-func _on_cell_weapon_changed(ability, weapon_value):
-	pass
+func _process_ability(ability) -> void:
+	var _ability = ability as Ability
+	var cell = AbilityCell.instance()
+	container.add_child(cell)
+	cell.init(_ability)
+	_abilities.append(cell)
+
+
+func _on_abilities_change(abilities: Array) -> void:
+	_clear_abilities()
+	_abilities = []
+	for a in abilities:
+		if not a: continue
+		_process_ability(a)
