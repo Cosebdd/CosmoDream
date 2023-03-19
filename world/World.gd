fe06e6208ada4de11a179bbfbbf516500774aac6
Cosebdd <Cosebdd@gmail.com>
2023-03-19 17:54:06 +0100

    Added 4 levels

--- world/World.gd
+++ world/World.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
+export(PackedScene) var nextScene
+
 onready var player := $Player
 onready var camera := $Camera2D
 
 func _ready() -> void:
 	player.connect_camera(camera)
+
+
+func _on_NextLevelArea_body_entered(body):
+	if body.get_name() == "Player":
+		get_tree().change_scene(nextScene.resource_path)
