ff3ef7f33ed3940d07abf70b20480660e13107f8
Viktor Pisarenko <xperia.v.j.p@gmail.com>
2023-03-30 23:10:38 +0200

    Remove onready statement for global configs

--- global/Config.gd
+++ global/Config.gd
@@ -1,21 +1,23 @@
 extends Node
 
 
-onready var player_config_data = {
+var player_config_data = {
 	"max_health": 4
 }
 
-onready var ability_config_data = {
+var ability_config_data = {
 	"ability_cell_number": 4,
 	"ability_max_health": 1
 }
 
+
 func get_player_max_health():
 	return player_config_data["max_health"]
-	
+
+
 func get_ability_cell_number():
 	return ability_config_data["ability_cell_number"]
-	
+
+
 func get_ability_max_health():
 	return ability_config_data["ability_max_health"]
-	
--- global/WorldState.gd
+++ global/WorldState.gd
@@ -1,39 +1,38 @@
 extends Node
 
 
-onready var ability_cell_number = Config.get_ability_cell_number()
-onready var ability_max_health = Config.get_ability_max_health()
-onready var player_max_health = Config.get_player_max_health()
-onready var ability_state = {
-					"ShootingAbility": ability_max_health, 
-					"Shield":          ability_max_health, 
-					"DubleJump":       ability_max_health, 
-					"StrafeAbility":   ability_max_health
-					}
-					
-onready var player_state = {
-					"health": player_max_health
-					}
+var ability_cell_number = Config.get_ability_cell_number()
+var ability_max_health = Config.get_ability_max_health()
+var player_max_health = Config.get_player_max_health()
+var ability_state = {
+	"ShootingAbility": ability_max_health, 
+	"Shield": ability_max_health, 
+	"DubleJump": ability_max_health, 
+	"StrafeAbility": ability_max_health
+}
 
+var player_state = {
+	"health": player_max_health
+}
 
 
 func _ready():
 	Events.connect("ability_gets_damage", self,  "_on_ability_gets_damage")
 	Events.connect("user_gets_damage", self, "_on_user_gets_damage")
 
+
 func _on_ability_gets_damage(ability, ability_index, damage):
 	var ability_name = ability.get_name()
 	ability_state[ability_name] = clamp(ability_state[ability_name] - damage, 0, ability.max_health)
-	
+
+
 func _on_user_gets_damage(damage):
 	player_state["health"] = clamp(player_state["health"] - damage, 0, player_max_health)
-	
-	
+
+
 func get_ability_state():
 	return ability_state
-	
+
+
 func get_player_health():
 	return player_state["health"]
-
-		
-
